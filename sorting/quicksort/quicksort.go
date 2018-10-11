2604d0dd08725e12543f11f6f3f144619b2b4a60
lihong <lihongitchina@gmail.com>
2018-10-11 16:37:50 +0800

    update quicksort

--- sorting/quicksort/quicksort.go
+++ sorting/quicksort/quicksort.go
@@ -17,7 +17,8 @@ func quickSort(data []int, L, R int) {
 }
 
 func partition(data []int, L, R int) (int, int) {
-	less, more := L-1, R
+	less, more := L-1, R //less左边为全部小于data[R]的值， less=L-1表示目前没有左边区域，而more= R len(data) -1 表示先直接把data[len(data)-1]值划分为大于data[R]对的第一个值， 所以more此时的边界已经包含data[R]了， 最后交互完成后，要注意把这个data[R]在交互到more位置，交互后more是就是等于data[R]的右边界, 而less+1就是等于data[R]的左边界
+
 	for L < more {
 		if data[L] < data[R] {
 			data[less+1], data[L] = data[L], data[less+1]
@@ -30,7 +31,7 @@ func partition(data []int, L, R int) (int, int) {
 			L++
 		}
 	}
-	//因为是取data[R]为比较值，所以最后要把R与more位置值交互一下，此时less左边全是小于data[R],more右边全是大于data[R],less+1, more中间全是等于data[R]
+	//因为是取data[R]为比较值，现在more的位置为第一个'大于'data[R]值的位置， 所以最后要把R与more位置值交互一下，此时less左边全是小于data[R],more右边全是大于data[R],less+1, more中间全是等于data[R]
 	data[R], data[more] = data[more], data[R]
 	return less + 1, more
 }
